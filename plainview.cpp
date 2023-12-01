0438e58265d6e58d4ca57e810a2a37126d3555ed
simon <nitrologic@gmail.com>
2023-12-01 18:16:58 +1300

    sdl3 ftw 1

--- plainview.cpp
+++ plainview.cpp
@@ -10,6 +10,8 @@ All Rights Reserved
 
 const char *plainviewVersion = "0.2";
 
+static bool terminateApp = false;
+
 #include <iostream>
 
 #define GLAD_GL_IMPLEMENTATION
@@ -121,7 +123,11 @@ void dumpModes() {
 }
 
 struct Driver {
+	int frameCount=0;
 	virtual int test() = 0;
+	virtual int addWindow(int w, int h, int hz, int flags) = 0;
+	virtual void closeWindow(int frame) = 0;
+	virtual void flip(int frame) = 0;
 	virtual void close() = 0;
 };
 
@@ -144,28 +150,113 @@ struct SDLDriver : Driver {
 		SDL_QuitSubSystem(sdlFlags);
 	}
 
-	int test() {
+	std::vector<SDL_Window*> windows;
+	std::vector<SDL_Surface*> surfaces;
+
+	void closeWindow(int frame) {
+		SDL_Window* window = windows[frame];
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+	}
+
+	int addWindow(int w,int h,int hz,int flags){
 		SDL_Window* window = NULL;
-		SDL_Surface* screenSurface = NULL;
-		Uint32 flags = SDL_WINDOW_OPENGL | SDL_WINDOW_HIGH_PIXEL_DENSITY;
-		window = SDL_CreateWindow("plainview", 1024, 768, flags);
+		window = SDL_CreateWindow("plainview", w, h, flags);
+		if (window == NULL) {
+			const char *error=SDL_GetError();
+			std::cout << "could not create window " << error << std::endl;
+			return -1;
+		}
+		int result = (int)windows.size();
+		windows.push_back(window);
+		SDL_Surface* surface = SDL_GetWindowSurface(window);
+		surfaces.push_back(surface);
+		return result;
+	}
 
+	void flip(int frame){
+		SDL_Surface *surface=surfaces[frame];
+
+		int g = frameCount++ % 100;
+
+		Uint32 c = SDL_MapRGB(surface->format, 44, g, 77);
+
+		SDL_FillSurfaceRect(surface, NULL, c);
+
+		SDL_Window *window=windows[frame];
+		SDL_UpdateWindowSurface(window);
+	}
+
+// https://discourse.libsdl.org/t/vsync-while-software-rendering-my-solution/26824
+
+	int test() {
+		Uint32 flags = SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL;	// | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+		int wh=addWindow(1280,960,75,flags);
+		if(wh<0){
+			std::cout << "addWindow failure" << std::endl;
+			return -1;
+		}
+		bool running = true;
+		while (running) {
+			int count = (frameCount++) % 100;
+			int g = count*2;
+			flip(wh);
+			SDL_Event event;
+			if (SDL_WaitEventTimeout(&event, 5)) {
+				switch (event.type) {
+				case SDL_EVENT_KEY_DOWN:
+					running = false;
+					break;
+				}
+			}
+			SDL_Delay(5);
+//			std::cout << "." << std::endl;
+		}
+		closeWindow(wh);
+	}
+
+	int test2() {
+		SDL_Window* window = NULL;
+		Uint32 flags = SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL;	// | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+
+		int w = 1280;
+		int h = 960;
+		int hz = 75;
+
+		window = SDL_CreateWindow("plainview", w, h, flags);
 		//		"hello_sdl2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,1024,768,SDL_WINDOW_SHOWN);
-
 		if (window == NULL) {
 			fprintf(stderr, "could not create window: %s\n", SDL_GetError());
 			return 1;
 		}
-		screenSurface = SDL_GetWindowSurface(window);
 
-		SDL_FillSurfaceRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-		SDL_UpdateWindowSurface(window);
-		SDL_Delay(2000);
+		SDL_Surface* screenSurface = NULL;
+		screenSurface = SDL_GetWindowSurface(window);
+		int frameCount = 0;
+
+		bool running = true;
+		while (running) {//!glfwWindowShouldClose(window)) {
+			int count = (frameCount++) % 100;
+			int g = count*2;
+			SDL_FillSurfaceRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 44, g, 77));
+			SDL_UpdateWindowSurface(window);
+			SDL_Event event;
+			if (SDL_WaitEventTimeout(&event, 5)) {
+				switch (event.type) {
+				case SDL_EVENT_KEY_DOWN:
+					running = false;
+					break;
+				}
+			}
+			SDL_Delay(5);
+//			std::cout << "." << std::endl;
+		}
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 	}
 
 };
+
 
 Driver *sdlOpen() {
 	SDLDriver *driver = new SDLDriver();
@@ -194,7 +285,9 @@ Driver *sdlOpen() {
 	return driver;
 }
 
-
+void glfwKeyHandler(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	terminateApp = true;
+}
 
 struct GLFWDriver :Driver {
 
@@ -206,40 +299,58 @@ struct GLFWDriver :Driver {
 		}
 		std::cout << "GLFW " << glfwGetVersionString() << std::endl;
 	}
+	virtual int addWindow(int w, int h, int hz, int flags) {
+		return -1;
+	}
+
+	virtual void closeWindow(int frame) {
+
+	}
+
+	virtual void flip(int frame) {
+
+	}
 
 	void close() {
 		glfwTerminate();
 	}
 
 	int test() {
-		GLFWwindow* window = glfwCreateWindow(640, 480, "GLFWDriver test window", NULL, NULL);
-
+//		int w = 1280;
+//		int h = 960;
+		int w = 720;
+		int h = 576;
+		int hz = 75;
+		glfwWindowHint(GLFW_REFRESH_RATE, hz);
+		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+		GLFWwindow* window = glfwCreateWindow(w, h, "GLFWDriver test window", monitor, NULL);
+//		glfwSetWindowMonitor(window, monitor, 0, 0, w, h, hz);
 		glfwMakeContextCurrent(window);
-
-		glfwSwapInterval(1);
-
 		if (!gladLoadGL(glfwGetProcAddress)) // For GLAD 2 use the following instead: 
 		{
 			glfwTerminate();
 			exit(EXIT_FAILURE);
 		}
-
-		while (!glfwWindowShouldClose(window)){
-			glClearColor(1, 1, 0, 1);
+		glfwSwapInterval(1);
+		glfwSetKeyCallback(window, glfwKeyHandler);
+		int frameCount = 0;
+		while (!terminateApp && !glfwWindowShouldClose(window)){
+			int count = (frameCount++);
+			float g = 0.2*sin(0.032*count);
+			glClearColor(0.2, g, 0, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 			glfwSwapBuffers(window);
 			glfwPollEvents();
+//			glfwWaitEventsTimeout(0.01);
 		}
-
 		glfwDestroyWindow(window);
-
+		glfwPollEvents();
 		return 0;
 	}
 };
 
 Driver* glfwOpen() {
 	GLFWDriver* driver = new GLFWDriver();
-
 	int monitorCount;
 	GLFWmonitor** monitors = glfwGetMonitors(&monitorCount);
 	GLFWmonitor* primary = glfwGetPrimaryMonitor();
@@ -273,21 +384,32 @@ int testDriver(Driver* driver) {
 	return driver->test();
 }
 
-int main() {
+int glfwMain() {
+	std::cout << "plainview " << plainviewVersion << std::endl;
+	Driver* glfwDriver = glfwOpen();
+	dumpModes();
+//	testDriver(glfwDriver);
+	//	glfwMain();
+	
+	testDriver(glfwDriver);
+
+	//sdlDriver->close();
+	glfwDriver->close();
+	return 0;
+}
+
+
+int sdlMain() {
 	std::cout << "plainview " << plainviewVersion << std::endl;
 
 	Driver *sdlDriver = sdlOpen();
-	Driver* glfwDriver = glfwOpen();
-
 	dumpModes();
-
-//	glfwMain();
-	
-//	testDriver(sdlDriver);
-	testDriver(glfwDriver);
-
-
+	testDriver(sdlDriver);
 	sdlDriver->close();
-
 	return 0;
+}
+
+
+int main() {
+	return sdlMain();
 }
