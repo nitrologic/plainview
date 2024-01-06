7d96143e15633d011ae763e96a6052dac394a36b
simon <nitrologic@gmail.com>
2024-01-07 10:40:13 +1300

    macos tweaks

--- shaders/rayGeometry.glsl
+++ shaders/rayGeometry.glsl
@@ -1,5 +1,8 @@
 #version 330
 
+flat in int vert_bits[];
+flat out int frag_bits;
+
 layout (triangles) in;
 
 layout (triangle_strip, max_vertices = 3) out;
@@ -8,8 +11,10 @@ void main()
 {    
 	for (int v = 0 ; v < 3; v++){
 		gl_Position = gl_in[v].gl_Position + vec4( 0.1, -0.1, 0.0, 0.0 ); 
+		frag_bits = vert_bits[v];
 		EmitVertex();
 	}
+
 	
 	EndPrimitive();
 } 
--- shaders/rayVertex.glsl
+++ shaders/rayVertex.glsl
@@ -4,9 +4,9 @@ uniform mat4 view;
 
 in vec3 xyz;
 
-in float bits;
+//in float bits;
 
-flat out int frag_bits;
+flat out int vert_bits;
 
 void main(){
 	vec4 v = vec4( xyz.x, xyz.y, xyz.z, 1.0 );
@@ -15,7 +15,7 @@ void main(){
 
 	vec4 i = vec4( vv.x, vv.y, 0.0, 1.0 );
 
-	frag_bits = int( bits );
+	vert_bits = 0;//int( bits );
 
 	gl_Position = i;
 }
